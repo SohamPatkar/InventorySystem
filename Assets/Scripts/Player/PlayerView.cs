2eb8252d87a0e3d03ef843b841f2ba8e427bb260
sohamPatkar <spatkar398@gmail.com>
2025-05-14 17:16:32 +0530

    Changed the architecture and deleted files

--- Assets/Scripts/Player/PlayerView.cs
+++ Assets/Scripts/Player/PlayerView.cs
@@ -4,18 +4,9 @@ public class PlayerView : MonoBehaviour
 {
     private PlayerController playerController;
 
-    private void Start()
-    {
-
-    }
-
     public void SetPlayerController(PlayerController playerController)
     {
         this.playerController = playerController;
     }
 
-    private void OnDisable()
-    {
-
-    }
 }
